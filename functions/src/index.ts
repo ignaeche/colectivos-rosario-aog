ff0eadebcdbc927c8e5222be683ade41d9c0816a
Ignacio <ignaeche@gmail.com>
2018-04-19 22:10:39 -0300

    Migration to API v2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,49 +1,41 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { isNull } from 'util';
-const { DialogflowApp } = require('actions-on-google');
+import { dialogflow } from 'actions-on-google';
 
 admin.initializeApp(functions.config().firebase)
 const db = admin.firestore()
 
-process.env.DEBUG = 'actions-on-google:*'
-
-const CUANDOLLEGA_ACTION = "cuandollega"
+const CUANDO_LLEGA_INTENT = "cuando_llega_intent"
 const BUS_LINE_ARGUMENT = "bus-line"
 const STREET_ARGUMENT = "street"
 const INTERSECTION_ARGUMENT = "intersection"
 const STOP_NUMBER_ARGUMENT = "stop-number"
 
-export const cuandoLlegaFulfillment = functions.https.onRequest((request, response) => {
-    const agent = new DialogflowApp({request, response})
-    console.log("cuandoLlegaFulfillment started")
+const app = dialogflow({debug: true})
 
-    function cuandollegaAction(app) {
-        console.log("Function cuandollegaAction called")
-        const bus = app.getArgument(BUS_LINE_ARGUMENT)
-        const stop = app.getArgument(STOP_NUMBER_ARGUMENT)
-        if (!isNull(stop)) {
-            app.ask(`La línea ${bus} llega a la parada ${stop} en 5 minutos.`)
-        } else {
-            const street = app.getArgument(STREET_ARGUMENT)
-            const intersection = app.getArgument(INTERSECTION_ARGUMENT)
-            db.collection('streets').where('desc', '==', street).get()
-                .then(snapshot => {
-                    if (snapshot.size === 0) {
-                        console.log(`Street not found in collection: ${street}`)
-                    } else {
-                        snapshot.forEach(doc => {
-                            console.log("Street found in streets collection: ", doc.data())
-                        })
-                    }
+app.intent(CUANDO_LLEGA_INTENT, conv => {
+    const bus = conv.parameters[BUS_LINE_ARGUMENT]
+    const stop = conv.parameters[STOP_NUMBER_ARGUMENT]
+    if (!isNull(stop)) {
+        conv.ask(`La línea ${bus} llega a la parada ${stop} en 5 minutos.`)
+    } else {
+        const street = conv.parameters[STREET_ARGUMENT]
+        const intersection = conv.parameters[INTERSECTION_ARGUMENT]
+        db.collection('streets').where('desc', '==', street).get()
+        .then(snapshot => {
+            if (snapshot.size === 0) {
+                console.log(`Street not found in collection: ${street}`)
+            } else {
+                snapshot.forEach(doc => {
+                    console.log("Street found in streets collection: ", doc.data())
                 })
-                .catch(error => console.error(error))
-            // search for stop number...
-            app.ask(`La línea ${bus} llega a la parada de ${street} y ${intersection} en 3 minutos.`)
-        }
+            }
+        })
+        .catch(error => console.error(error))
+        // search for stop number...
+        conv.ask(`La línea ${bus} llega a la parada de ${street} y ${intersection} en 3 minutos.`)
     }
+})
 
-    const actionMap = new Map()
-    actionMap.set(CUANDOLLEGA_ACTION, cuandollegaAction)
-    agent.handleRequest(actionMap)
-});
+export const cuandoLlegaFulfillment = functions.https.onRequest(app)
