d43499b393259fd8fabff2abc61f4c4c5ef625e1
Ignacio <ignaeche@gmail.com>
2018-05-13 04:50:01 -0300

    More organizing responses and translations

--- functions/src/responses.ts
+++ functions/src/responses.ts
@@ -1,5 +1,5 @@
 import i18next from './i18next';
-import { SimpleResponse, Suggestions, BasicCard, BasicCardOptions, Image, Button, OptionItem } from 'actions-on-google';
+import { SimpleResponse, Suggestions, BasicCard, BasicCardOptions, Image, Button, OptionItem, SimpleResponseOptions } from 'actions-on-google';
 import { BusArrival, ArrivalTranslation, Corner, ArrivalTime, Bus, Stop, Street } from './models';
 import { randomPop, takeRandom } from './util';
 import { getStopLocationImage, getStopMapsLink } from './maps';
@@ -10,18 +10,20 @@ function createSimpleResponse(key: string, data) {
     return new SimpleResponse(i18next.t(key, data))
 }
 
-function determineArrival(keys: string[], time: ArrivalTime) {
+function determineArrival(key: string, time: ArrivalTime) {
+    const deepKeys = ['arriving', 'arrivesInScheduled', 'arrivesIn'].map(k => `${key}.${k}`)
     if (time.arriving) {
-        return i18next.t(keys[0])
+        return i18next.t(deepKeys[0])
     } else {
-        let key;
-        if (time.scheduled) {
-            key = keys[1]
-        } else {
-            key = keys[2]
-        }
-        return i18next.t(key, { minutes: time.minutes })
+        return i18next.t(time.scheduled ? deepKeys[1] : deepKeys[2], { minutes: time.minutes })
     }
+}
+
+const stopMapImage = (stop: Stop, size?) => {
+    return new Image({
+        url: getStopLocationImage(stop.location, i18next.language, size),
+        alt: i18next.t('stop.number', { stop: stop.number }),
+    })
 }
 
 export const negatives = {
@@ -62,10 +64,7 @@ export const items = {
             description: i18next.t(descriptionKey, tOptions)
         }
         if (stop.location) {
-            item.image = new Image({
-                url: getStopLocationImage(stop.location, i18next.language, 'LIST_SIZE'),
-                alt: i18next.t('stop.number', { stop: stop.number })
-            })
+            item.image = stopMapImage(stop, 'LIST_SIZE')
         }
         return item
     },
@@ -77,44 +76,43 @@ export const items = {
     }
 }
 
-export const prompts = {
-    'foundArrivalTimes': (bus: Bus, corner: Corner) => {
-        return new SimpleResponse(i18next.t('foundArrivalTime', {
-            bus: bus.name,
+export const arrivals = {
+    'foundTimes': (corner: Corner) => {
+        return createSimpleResponse('arrivals.foundTimes', {
+            bus: corner.bus.name,
             street: corner.stop.street.desc,
             intersection: corner.stop.intersection.desc,
             stop: corner.stop.number
-        }))
+        })
     },
-    'arrivalTimes': (arrivalTimes: Map<string, BusArrival[]>) => {
-        const response = []
-        Object.keys(arrivalTimes).forEach(flag => { 
-        // arrivalTimes.forEach((arrivals: BusArrival[], flag: string) => {
-            const arrivals = arrivalTimes[flag]
-            const responseLine = []
-            const data: ArrivalTranslation = {} as ArrivalTranslation
-            data.flag = flag
+    'times': (times: Map<string, BusArrival[]>) => {
+        const response: Array<Array<SimpleResponseOptions>> = []
+        Object.keys(times).forEach(flag => {
+            const busArrivals: BusArrival[] = times[flag]
+            const line = []
             // Get first arrival
-            const arrival = arrivals.shift()
-            // Get arrival string
-            data.arrivesIn = determineArrival(['arrivalIsArriving', 'arrivalInMinutesScheduled', 'arrivalInMinutes'], arrival.time)
-            responseLine.push(i18next.t('arrival', data))
-
-            // Get next arrival, if exists will append to message
-            const next = arrivals.shift()
-            if (next !== undefined) {
-                const nextData = { arrivesIn: determineArrival(['arrivalNextIsArriving', 'arrivalNextScheduled', 'arrivalNext'], next.time) }
-                responseLine.push(i18next.t('nextBus', nextData))
+            const arrival = busArrivals.shift()
+            const data = {
+                flag,
+                arrivesIn: determineArrival('arrivals.bus', arrival.time) // get arrival string
             }
-            response.push(responseLine)
+            line.push(i18next.t('arrivals.sentences.firstBus', data))
+            // Get next arrival, if exists will append to message
+            const next = busArrivals.shift()
+            if (next !== undefined) {
+                line.push(i18next.t('arrivals.sentences.nextBus', { arrivesIn: determineArrival('arrivals.nextBus', next.time) }))
+            }
+            // Push line to final response
+            response.push(line)
         })
+        const flatten = (key, inner, outer) => response.map(l => l.map(o => o[key]).join(inner)).join(outer)
         return new SimpleResponse({
-            speech: addSSMLTag(response.map(l => l.map(o => o.speech).join("")).join("<break time=\"400ms\"/>"), 'speak'),
-            text: response.map(l => l.map(o => o.text).join(" ")).join(" ")
+            speech: wrapTag(flatten('speech', '', '<break time=\"400ms\"/>'), 'speak'),
+            text: flatten('text', ' ', ' ')
         })
     },
-    'arrivalTimesComplete': (bus: Bus, corner: Corner, arrivalTimes: Map<string, BusArrival[]>) => {
-        return [prompts.foundArrivalTimes(bus, corner), prompts.arrivalTimes(arrivalTimes)]
+    'completeAnswer': (corner: Corner, times: Map<string, BusArrival[]>) => {
+        return [arrivals.foundTimes(corner), arrivals.times(times)]
     }
 }
 
@@ -126,10 +124,7 @@ export const rich = {
             text: i18next.t('stop.card.text', { stop })
         }
         if (stop.location) {
-            options.image = new Image({
-                url: getStopLocationImage(stop.location, i18next.language),
-                alt: i18next.t('stop.number', { stop: stop.number }),
-            })
+            options.image = stopMapImage(stop)
             options.display = 'CROPPED'
             options.buttons = new Button({
                 title: i18next.t('viewOnGoogleMaps'),
@@ -153,6 +148,6 @@ export const suggestions = {
     }
 }
 
-function addSSMLTag(string: string, tag: string) {
+function wrapTag(string: string, tag: string) {
     return `<${tag}>${string}</${tag}>`
 }
